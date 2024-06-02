db104037e7848954cc594209b648a4fd1e369257
DS-lyra8 <154302053+DS-lyra8@users.noreply.github.com>
2024-06-02 23:36:37 +0900

    Update dashboard_main_code.py

--- dashboard_main_code.py
+++ dashboard_main_code.py
@@ -11,20 +11,20 @@ import seaborn as sns
 import tkinter.ttk as ttk
 
 #def code import
-from dashboard_code.plot_logi_feas_creat_poss import plot_logi_feas_creat_poss
-from dashboard_code.plot_chart_achieve_view import plot_chart_achieve_view
-from dashboard_code.set_title import set_title
-from dashboard_code.plot_score_date import plot_score_date
-from dashboard_code.plot_student_radar import plot_student_radar
+from plot_logi_feas_creat_poss import plot_logi_feas_creat_poss
+from plot_chart_achieve_view import plot_chart_achieve_view
+from set_title import set_title
+from plot_score_date import plot_score_date
+from plot_student_radar import plot_student_radar
 
 COLUMN = 'column'  # 실제 사용되는 상수 값을 확인하여 적절히 정의
 
 
 
 # 데이터프레임 로드
-df_achievement = pd.read_csv('student_data/final_user_g3.csv')  
-df_projectScore = pd.read_csv('student_data/final_merged_project_g3.csv')
-# df_projectScore = pd.read_csv('project_result.csv')
+df_achievement  = pd.read_csv('achivement_user.csv')
+df_projectScore = pd.read_csv('achivement_project.csv')
+
 
 # 'userid' 열의 데이터 타입을 문자열(string)로 설정
 df_achievement['userid'] = df_achievement['userid'].astype(str)
@@ -75,16 +75,16 @@ def main_page():
         fig, _ = plot_chart_achieve_view(df_achievement, 'visit', "우리반 조회수 1등")
         st.plotly_chart(fig, use_container_width=True)
 
-    with col4:
+    with col4:  # 학급 내 논리성 점수 시각화
         plot_logi_feas_creat_poss(df_achievement, 'logicalRate', '논리성', 'logicalRate', 'logicalRate')
         
-    with col5:
+    with col5:  # 학급 내 구현성 점수 시각화
         plot_logi_feas_creat_poss(df_achievement, 'feasibilityRate', '구현성', 'feasibilityRate', 'feasibilityRate')
         
-    with col6:
+    with col6: # 학급 내 창의성 점수 시각화
         plot_logi_feas_creat_poss(df_achievement, 'creativityRate', '창의성', 'creativityRate', 'creativityRate')
         
-    with col7:
+    with col7:  # 학급 내 적극성 점수 시각화
         plot_logi_feas_creat_poss(df_achievement, 'positivenessRate', '적극성', 'positivenessRate', 'positivenessRate')
 
     with empty2:
@@ -101,10 +101,10 @@ def create_user_page(user_id):
         with col8:
             set_title(f"{user_id}의 세부 평가지표 대시보드")
                    
-        with col9:
+        with col9:  # 학생 점수 레이더 차트 시각화
             plot_student_radar(df_achievement, user_id)
     
-        with col10:
+        with col10:  # 학생 성취도 점수, 등수 시각화
             st.markdown(f'**2. "{user_id}" 학생의 등수**')
             fig, result_text = plot_chart_achieve_view(df_achievement, 'achievementScore', "반 전체 성취도 점수", user_id=user_id)
             st.plotly_chart(fig, use_container_width=True)
@@ -115,7 +115,6 @@ def create_user_page(user_id):
         with col11:
             st.markdown(f'**3. "{user_id}" 학생의 날짜 별 평가지표 점수 추이**')
         
-        # 프로젝트별 성취도 점수 추이 시각화
         with col12:
             # logicalScore 점수를 date 별로 나타내는 산점도 그래프
             plot_score_date(df_projectScore, user_id, 'logical', f"{user_id}의 논리성 점수 추이")
